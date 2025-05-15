cecc69bd8fcb7d68fa76fd228f994488fd451a6d
aovictor <aovictor@gmail.com>
2025-05-14 17:14:35 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -26,19 +26,19 @@ echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> ~/.zshrc
 eval "$(/opt/homebrew/bin/brew shellenv)"
 brew bundle
 
-# install atom monokai theme
-/usr/local/bin/apm install atom-monokai
-
 # Install Oh-My-zsh ##################################################################################
 /bin/sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-/usr/bin/git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
-# Install oh-my-zsh fonts
-mkdir ~/git
-/usr/bin/git clone https://github.com/Karmenzind/monaco-nerd-fonts.git ~/git/monaco-nerd-fonts
-/bin/cp ~/git/monaco-nerd-fonts/fonts/* /Library/Fonts
+# install theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git "${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k"
+# install font
+curl -L https://github.com/romkatv/powerlevel10k-media/raw/master/MesloLGS%20NF%20Regular.ttf -o ~/Library/Fonts/MesloLGS\ NF\ Regular.ttf
+
 # Install oh-my-zsh configs
 /bin/cp -f zshrc ~/.zshrc
-echo "oh-my-zsh installed, safe to open with iterm."
+/bin/cp -f p10k.zsh ~/.p10k.zsh
+mkdir -p ~/.config/ghostty
+/bin/cp ghostty_config ~/.config/ghostty/config
+echo "oh-my-zsh installed"
 
 # Install asdf plugins
 echo -e "\n. $(brew --prefix asdf)/asdf.sh" >> ~/.zshrc
@@ -46,14 +46,3 @@ asdf plugin-add golang
 asdf plugin-add ruby
 asdf plugin-add python
 asdf plugin-add terraform
-
-# Create apps #########################################################################################
-# Install natifier
-/usr/bin/git clone https://github.com/jiahaog/nativefier.git ~/git/nativefier
-brew install node
-cd ~/git/nativefier
-npm install nativefier -g && echo "nativefier installed"
-# Create gmail
-nativefier -n "gMail" --internal-urls '.*.google.com.*' 'http://mail.google.com'
-/bin/mv -fv $(find . -name "gMail.app") /Applications/ && echo "gMail.app installed."
-/bin/rm -Rf $(find . -name gMail*)
